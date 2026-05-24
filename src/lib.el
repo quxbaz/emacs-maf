667b38fe8c142283b061ad8099a53125bbea66fa
David Yeung <quxbaz@gmail.com>
2026-05-23 23:23:28 -0600

    modified:   src/lib.el

---
+++ src/lib.el
@@ -0,0 +1,22 @@
+;; -*- lexical-binding: t; -*-
+;;
+;; lib.el
+;;
+;; maf library functions
+
+
+(defun maf--get-calc-buffer ()
+  "Find the calc buffer.
+Prefers the current buffer if it is in calc-mode, then looks for
+*Calculator* by name, then falls back to any live buffer in calc-mode."
+  (cond
+   ((derived-mode-p 'calc-mode) (current-buffer))
+   ((get-buffer "*Calculator*"))
+   (t (cl-find-if (lambda (buf)
+                    (with-current-buffer buf (derived-mode-p 'calc-mode)))
+                  (buffer-list)))))
+
+(defun maf--at-home-p ()
+  "Return t if point is past the last stack entry (at the . line or below)."
+  (with-current-buffer (my/calc-find-buffer)
+    (<= (calc-locate-cursor-element (point)) 0)))
