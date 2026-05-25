843c06ad0d637f9bb0c8cb2feb1884e49443a04f
David Yeung <quxbaz@gmail.com>
2026-05-25 16:13:17 -0600

    modified:   src/defcmd.el

--- src/defcmd.el
+++ src/defcmd.el
@@ -52,10 +52,10 @@ Possible :target values, in order of priority:
   equation   Entry is a relation (=, !=, <, <=, >, >=); body runs once per side.
   entry      Whole stack entry; point is at EOL, line-prefix zone, or line mode is forced."
   (maf--with-calc-buffer
-    (let ((keep calc-keep-args-flag)
-          (arity (alist-get :arity opts))
-          (unary? (eq arity 'unary))
-          (binary? (eq arity 'binary)))
+    (let* ((keep calc-keep-args-flag)
+           (arity (alist-get :arity opts))
+           (unary? (eq arity 'unary))
+           (binary? (eq arity 'binary)))
       (append (cond ((maf--at-home-p) `((:target . home)
                                         (:expr   . ,(calc-top 1 'full))
                                         (:arg    . ,(when binary? (calc-top 2 'full)))
