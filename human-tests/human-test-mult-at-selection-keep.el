589442491ad0795ea596ff8643e983edface6649
David Yeung <quxbaz@gmail.com>
2026-05-26 02:40:51 -0600

    modified:   human-tests/human-test-mult-at-selection-keep.el

---
+++ human-tests/human-test-mult-at-selection-keep.el
@@ -0,0 +1,19 @@
+(maf-defcmd maf-mult (expr arg commit)
+  "Multiplication command."
+  :arity binary
+  :prefix "mult"
+  (commit (calcFunc-mul expr arg)))
+
+(maf--debug-setup-test)
+
+(maf--debug-slowly 0.3
+  (calc-push '(+ (* 20 (var x var-x)) 10))
+  (progn
+    (calc-push 2)
+    (calc-refresh))
+  (goto-char 7)
+  (call-interactively 'calc-select-here)
+  (progn
+    (call-interactively 'calc-keep-args)
+    (call-interactively 'maf-mult))
+  (calc-clear-selections))
