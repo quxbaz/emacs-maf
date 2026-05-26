c11e6fee08558ce4b2af776d4a519d0d6780623a
David Yeung <quxbaz@gmail.com>
2026-05-26 02:35:02 -0600

    modified:   human-tests/human-test-square-at-home-keep.el

---
+++ human-tests/human-test-square-at-home-keep.el
@@ -0,0 +1,12 @@
+(maf-defcmd maf-square (expr arg commit)
+  "Square command."
+  :arity unary
+  :prefix "sqr"
+  (commit (calcFunc-mul expr expr)))
+
+(maf--debug-setup-test)
+
+(maf--debug-slowly 0.3
+  (calc-push 4)
+  (call-interactively 'calc-keep-args)
+  (call-interactively 'maf-square))
