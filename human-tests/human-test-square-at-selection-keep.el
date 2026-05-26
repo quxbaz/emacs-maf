3284f58f1fdece070f02c5ae6331466bf6eaf679
David Yeung <quxbaz@gmail.com>
2026-05-26 02:35:50 -0600

    modified:   human-tests/human-test-square-at-selection-keep.el

---
+++ human-tests/human-test-square-at-selection-keep.el
@@ -0,0 +1,18 @@
+(maf-defcmd maf-square (expr arg commit)
+  "Square command."
+  :arity unary
+  :prefix "sqr"
+  (commit (calcFunc-mul expr expr)))
+
+(maf--debug-setup-test)
+
+(maf--debug-slowly 0.3
+  (calc-push '(+ (* 10 (var x var-x)) 4))
+  (progn
+    (calc-push '(var c var-c))
+    (calc-refresh))
+  (goto-char 7)
+  (call-interactively 'calc-select-here)
+  (call-interactively 'calc-keep-args)
+  (call-interactively 'maf-square)
+  (calc-clear-selections))
