0449737eaeda4621e11fd3fe0dba7e4a9043b9ee
David Yeung <quxbaz@gmail.com>
2026-05-28 21:02:06 -0600

    modified:   manual-tests/step-test-mult-at-equation.el

--- manual-tests/step-test-mult-at-equation.el
+++ manual-tests/step-test-mult-at-equation.el
@@ -11,9 +11,5 @@
   (calc-push 2)
   (calc-cursor-stack-index 2)
   (call-interactively 'maf-mult)
-  (unless (= (calc-stack-size) 1)
-    (error "FAIL mult-at-equation: expected size 1, got %d" (calc-stack-size)))
-  (unless (string= (math-format-value (calc-top 1 'full)) "2 x = 10")
-    (error "FAIL mult-at-equation: expected '2 x = 10', got %s"
-           (math-format-value (calc-top 1 'full))))
-  (message "PASS mult-at-equation"))
+  (cl-assert (= (calc-stack-size) 1))
+  (cl-assert (string= (math-format-value (calc-top 1 'full)) "2 x = 10")))
